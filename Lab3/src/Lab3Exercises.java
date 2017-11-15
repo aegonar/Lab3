0bef01878af6eb44d7a8c05eb418501ea6fe6e9a
alejandro.gonzalez3 <alejandro.gonzalez3@manzana.ece.uprm.edu>
2017-11-15 13:39:04 -0400

    Test

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,8 +2,8 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		JFrame myFrame = new JFrame("Hello, git!!");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(200, 200);
 		myFrame.setSize(400, 100);
 		myFrame.setVisible(true);
